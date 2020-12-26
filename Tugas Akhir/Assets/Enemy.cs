051485065aa8b87df6a8d09453baf078307b8247
zahwawr <zahwa_riana31@apps.ipb.ac.id>
2020-12-26 21:21:52 +0700

    hero, 1 enemy, level blm didisen, banyak bug

---
+++ Tugas Akhir/Assets/Enemy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Animator animator;
+    public int maxHealth = 100;
+
+    int currentHealth;
+
+    // Update is called once per frame
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        animator.SetTrigger("isHit");
+        if(currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("Enemy died!");
+        Destroy(gameObject, 2);
+        animator.SetBool("isDead", true);
+        GetComponent<Collider2D>().enabled = false;
+        this.enabled = false;
+    }
+}
---
+++ Tugas Akhir/Assets/PlayerCombat.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+
+    public Animator animator;
+    public Transform AttackPoint;
+    public float attackRange = 0.5f;
+    public int attackDamage = 40;
+    public LayerMask enemyLayers;
+
+    public float attackRate = 4f;
+    float nextAttackTime = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time >= nextAttackTime)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Attack();
+                nextAttackTime = Time.time + 1f / attackRate;
+            }
+        }
+    }
+
+    void Attack()
+    {
+        animator.SetTrigger("Attack");
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(AttackPoint.position, attackRange, enemyLayers);
+
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (AttackPoint == null)
+            return;
+
+        Gizmos.DrawWireSphere(AttackPoint.position, attackRange);
+    }
+}
